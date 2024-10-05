6ed3a9601a931dbe0f412eaf9077f8967a98cf99
JCarlosHidalgoSosa <juan.hidalgo.8798478@usalesiana.edu.bo>
2024-10-05 17:54:29 -0400

    bug solved: when redireting to sub-paths from dotnet service, application redirects according to donet service

--- pocs/nginx/3_load-balancer/csharp-service/Pages/Error.cshtml.cs
+++ pocs/nginx/3_load-balancer/csharp-service/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace csharpservice.Pages;
+namespace csharp_service.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
--- pocs/nginx/3_load-balancer/csharp-service/Pages/Index.cshtml.cs
+++ pocs/nginx/3_load-balancer/csharp-service/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace csharpservice.Pages;
+namespace csharp_service.Pages;
 
 public class IndexModel : PageModel
 {
