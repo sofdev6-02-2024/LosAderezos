fc9e8dd6eddf87644a917717ceef380d16eb2c0e
AngheloZambrana <anghelozambrana2005@gmail.com>
2024-10-23 14:21:32 -0400

    fix:Deleting extre carpet

--- apps/ProductManagement/ProductManagement/DB/DAOS/Abstract/ITwoForeignDAO.cs
+++
@@ -1,7 +0,0 @@
-namespace DB;
-
-public interface ITwoForeignDAO <T> : IDAO<T>
-{
-    T? Read(Guid id1, Guid id2);
-    bool Delete(Guid id1,Guid id2);
-}
--- apps/ProductManagement/ProductManagement/DB/Injectors/Abstract/IDataInjector.cs
+++
@@ -1,8 +0,0 @@
-using MySql.Data.MySqlClient;
-
-namespace DB;
-
-public interface IDataInjector
-{
-    int InjectData(MySqlConnection connection);
-}
