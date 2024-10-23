7bfa4e931887056207f490a31b054dc9993b533c
Anghelo Zambrana <101211793+AngheloZambrana@users.noreply.github.com>
2024-10-23 14:36:05 -0400

    Delete apps/ProductManagement/ProductManagement directory

--- apps/ProductManagement/ProductManagement/DB/DAOS/Abstract/IDAO.cs
+++
@@ -1,8 +0,0 @@
-namespace DB;
-
-public interface IDAO<T>
-{
-    int Create(T element);
-    List<T> ReadAll();
-    int Update(T element);
-}
--- apps/ProductManagement/ProductManagement/DB/DAOS/Abstract/ISingleDAO.cs
+++
@@ -1,7 +0,0 @@
-namespace DB;
-
-public interface ISingleDAO<T> : IDAO<T>
-{
-    T? Read(Guid id);
-    bool Delete(Guid id);
-}
