1c01c374faa5b74037223cbb0976f4ff6e733996
DeidamiaFuentes <deidamia.fuentes.8698857@usalesiana.edu.bo>
2024-10-29 15:13:57 -0400

    feat: header

--- apps/frontend/src/layouts/HeaderBussines.jsx
+++ apps/frontend/src/layouts/HeaderBussines.jsx
@@ -2,8 +2,9 @@ import { IoMdArrowBack } from "react-icons/io";
 import { CgBell } from "react-icons/cg";
 import Button from "../components/Button";
 import { useNavigate, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function HeaderBussines()
+export default function HeaderBussines({bussines, branch})
 {
   const navigate = useNavigate();
   const location = useLocation();
@@ -24,14 +25,14 @@ export default function HeaderBussines()
       </div>
       <div className="flex flex-col justify-center w-full font-roboto space-y-3">
         <p className="text-[32px] text-white font-medium md:font-bold w-full text-center truncate">
-          Nombre de empresa
+          {bussines}
         </p>
         <Button className={'w-full bg-white justify-center py-[8px] text-[20px] font-bold rounded-[10px]'}
         onClick={() => {
           navigate('*')
         }}
         >
-          Sucursal 1
+          {branch}
         </Button>
       </div>
       <div className="flex flex-row w-full justify-end">
@@ -45,4 +46,9 @@ export default function HeaderBussines()
       </div>
     </div>
   )
+}
+
+HeaderBussines.propTypes = {
+  bussines: PropTypes.string,
+  branch: PropTypes.string,
 }
