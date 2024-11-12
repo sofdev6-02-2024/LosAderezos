970abb9d2ecf9bcf72a08b2787c98a57e16a94c8
AngheloZambrana <anghelozambrana2005@gmail.com>
2024-11-12 15:12:27 -0400

    fix: init.sql

--- infrastructure/environments/product-management/productDB/init-db/init.sql
+++ infrastructure/environments/product-management/productDB/init-db/init.sql
@@ -142,6 +142,7 @@ DELIMITER //
 CREATE PROCEDURE GetProductCategoriesByProductId(IN productId CHAR(36))
 BEGIN
   SELECT
+    pc.ProductId
     pc.CategoryId
   FROM
     ProductCategories pc
@@ -155,6 +156,7 @@ CREATE PROCEDURE GetProductCategoriesByCategoryId(IN categoryId CHAR(36))
 BEGIN
   SELECT
     pc.ProductId
+    pc.CategoryId
   FROM
     ProductCategories pc
   WHERE
